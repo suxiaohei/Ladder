477c797dfe4365d0337c44621bedc94ef5a20c0d
苏鑫 <suxin@jeejen.com>
2018-01-25 21:12:49 +0800

    比较Integer大小,了解缓存机制

---
+++ genera/src/main/java/com/study/Ladder/example/DoubleCase.java
@@ -0,0 +1,12 @@
+package com.study.Ladder.example;
+
+public class DoubleCase {
+
+    public static void main(String[] args) {
+        Double d1 = 3.0D;
+        Double d2 = 3.0D;
+
+        System.out.println(d1 == d2);
+    }
+}
+
